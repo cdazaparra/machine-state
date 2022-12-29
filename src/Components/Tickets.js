4e87a1eed97a2575f3f202351f57d58802067d6f
Andres Daza <cdazaparra@gmail.com>
2022-12-29 09:45:48 -0500

    pull

--- src/Components/Tickets.js
+++ src/Components/Tickets.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Tickets.css';
 
-export const Tickets = ({ send, context }) => {
+export const Tickets = ({ send, state }) => {
   const finish = () => {
     send('FINISH')
   };
@@ -10,15 +10,18 @@ export const Tickets = ({ send, context }) => {
     <div className='Tickets'>
       <p className='Tickets-description description'>Gracias por volar con book a fly 💚</p>
       <div className='Tickets-ticket'>
-        <div className='Tickets-country'>Colombia</div>
+        <div className='Tickets-country'>{ state.context.selectedCountry }</div>
         <div className='Tickets-passengers'>
           <span>✈</span>
-          {context.passengers.map((person, idx) => {
-            return <p key={idx}>{person}</p>
-          })}
+          { state.context.passengers.map(person => (
+            <p key={person}>{person}</p>
+          )) }
         </div>
       </div>
-      <button onClick={finish} className='Tickets-finalizar button'>Finalizar</button>
+      <button 
+        onClick={finish} 
+        className='Tickets-finalizar button'
+      >Finalizar</button>
     </div>
   );
 }; 
